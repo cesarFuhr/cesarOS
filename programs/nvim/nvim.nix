286a832de060d5371d2fd016d8292f37b9a7523f
cesarFuhr <cesar.cara@protonmail.com>
2022-07-06 14:45:10 -0300

    fixed nvim config path

--- programs/nvim/nvim.nix
+++ programs/nvim/nvim.nix
@@ -6,12 +6,12 @@
   # So I am gonna load the files in the config dir directly.
   # Loading config files in the $XDG_CONFIG_HOME folder.
   xdg.configFile."nvim/init.lua".source = ./init.lua;
-  xdg.configFile."nvim/lua/_telescope.lua".source = ./nvim/lua/_telescope.lua;
-  xdg.configFile."nvim/lua/keymaps.lua".source = ./nvim/lua/keymaps.lua;
-  xdg.configFile."nvim/lua/lsp.lua".source = ./nvim/lua/lsp.lua;
-  xdg.configFile."nvim/lua/plugins.lua".source = ./nvim/lua/plugins.lua;
-  xdg.configFile."nvim/lua/settings.lua".source = ./nvim/lua/settings.lua;
-  xdg.configFile."nvim/lua/snip.lua".source = ./nvim/lua/snip.lua;
-  xdg.configFile."nvim/lua/theme.lua".source = ./nvim/lua/snip.lua;
+  xdg.configFile."nvim/lua/_telescope.lua".source = ./lua/_telescope.lua;
+  xdg.configFile."nvim/lua/keymaps.lua".source = ./lua/keymaps.lua;
+  xdg.configFile."nvim/lua/lsp.lua".source = ./lua/lsp.lua;
+  xdg.configFile."nvim/lua/plugins.lua".source = ./lua/plugins.lua;
+  xdg.configFile."nvim/lua/settings.lua".source = ./lua/settings.lua;
+  xdg.configFile."nvim/lua/snip.lua".source = ./lua/snip.lua;
+  xdg.configFile."nvim/lua/theme.lua".source = ./lua/snip.lua;
 }
 
