1bc736a1a1dc8de936d5622db9bdbd3e480a3991
cesarFuhr <cesar.cara@protonmail.com>
2022-07-06 14:48:46 -0300

    rofi theme

--- programs/rofi/rofi.nix
+++ programs/rofi/rofi.nix
@@ -8,10 +8,20 @@
     extraConfig = {
       modi = "window,drun,ssh";
       combi-modi = "window,drun";
+
+      show-icons = true;
+      icon-theme = "papirus";
+
+      window-format = "  {c}    {t}";
+      combi-display-format = "  {text}";
+      display-combi = "";
+
+      drun-display-format = "{name}";
     };
 
-    theme = "./spotlight-dark.rasi";
+    theme = "./theme.rasi";
   };
+
+  # Load the theme in the config folder.
+  xdg.configFile."rofi/theme.rasi".source = ./theme.rasi;
 }
-
-
