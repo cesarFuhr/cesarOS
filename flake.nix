9e10cb17dc36bf1948cbc0644bbf903ce700eb15
cesarFuhr <cesar.cara@protonmail.com>
2022-07-07 17:27:26 -0300

    better folder structure

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.cesar = import ./home.nix;
+              home-manager.users.cesar = import ./home/home.nix;
             }
           ];
         };
