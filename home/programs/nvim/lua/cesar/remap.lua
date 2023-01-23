fd2cdc38a20f540e7da400a6bd341e946863fbaf
Cesar Cara <cesar.cara@protonmail.com>
2023-01-23 09:50:20 -0300

    nvim remaps

--- home/programs/nvim/lua/cesar/remap.lua
+++ home/programs/nvim/lua/cesar/remap.lua
@@ -20,6 +20,28 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- No one should be here.
+vim.keymap.set("n", "Q", "<Nop>")
+vim.keymap.set("v", "Q", "<Nop>")
+
+-- Move up and down the highlighted lines.
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in place when appending lines.
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in the middle when jumping.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Replace word under the cursor.
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 -- Quickfix
 -- Go to quickfix window.
 vim.keymap.set('n', '<leader>ql', '<cmd>copen<cr>', opts)
