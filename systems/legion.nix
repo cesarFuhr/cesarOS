4b7dc63e9983295aeab0c857e00c7feed8f63cdb
Cesar Cara <cesar.cara@protonmail.com>
2024-09-06 11:03:15 -0300

    steam and better vsync

--- systems/legion.nix
+++ systems/legion.nix
@@ -98,6 +98,23 @@
       # Video drivers
       # Nvidia
       videoDrivers = [ "nvidia" ];
+
+      screenSection = ''
+        Identifier     "Screen0"
+        Device         "Device0"
+        Monitor        "Monitor0"
+        DefaultDepth    24
+        Option         "Stereo" "0"
+        Option         "nvidiaXineramaInfoOrder" "HDMI-0"
+        Option         "metamodes" "DP-4: nvidia-auto-select +3840+1080, HDMI-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=On}"
+        Option         "SLI" "Off"
+        Option         "MultiGPU" "Off"
+        Option         "BaseMosaic" "off"
+        SubSection     "Display"
+            Depth       24
+        EndSubSection
+      '';
+
       dpi = lib.mkForce 120;
 
       displayManager = {
@@ -299,6 +316,12 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
+  programs.steam = {
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
 
   # Open ports in the firewall.
   networking.firewall = lib.mkMerge [
