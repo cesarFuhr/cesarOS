97e56c8f1c39e9dcd1edc1b0dd3298bb1efcef1f
Cesar Cara <cesar.cara@protonmail.com>
2024-12-23 19:57:44 -0300

    removing steam from rogstrix

--- systems/rogstrix.nix
+++ systems/rogstrix.nix
@@ -316,12 +316,12 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-  };
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true;
+  #   dedicatedServer.openFirewall = true;
+  #   localNetworkGameTransfers.openFirewall = true;
+  # };
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 11111 ];
