bfeca655619d71ceadf02096adf258a12c0706e5
Cesar Cara <cesar.cara@protonmail.com>
2025-08-06 13:22:10 -0300

    fixing initialization logs leaking into the greeter

--- systems/rogstrix.nix
+++ systems/rogstrix.nix
@@ -138,6 +138,18 @@
       # Nvidia
       videoDrivers = [ "nvidia" ];
     };
+
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 
   console.useXkbConfig = true;
