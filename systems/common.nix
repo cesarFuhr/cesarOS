26123102b8933ef58fdffa78315304d1f53551cc
Cesar Cara <cesar.cara@protonmail.com>
2026-03-01 19:56:29 -0300

    tailscale at boot

--- systems/common.nix
+++ systems/common.nix
@@ -29,6 +29,7 @@
   # Enable networking
   networking = {
     nameservers = [
+      "100.100.100.100"
       "8.8.8.8"
       "1.1.1.1"
     ];
@@ -398,6 +399,39 @@
   ];
 
   services.tailscale.enable = true;
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wants = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      # if status is not null, then we are already authenticated
+      echo "tailscale status: $status"
+      if [ "$status" != "NeedsLogin" ]; then
+          exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      # timeout after 10 seconds to avoid hanging the boot process
+      ${coreutils}/bin/timeout 10 ${tailscale}/bin/tailscale up 
+    '';
+  };
 
   # Bluetooth
   services.blueman.enable = true;
